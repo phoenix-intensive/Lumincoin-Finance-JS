ebb486ecf5c682375333f8af0bd6b439c29b904c
Dmitriy <148681959+phoenix-intensive@users.noreply.github.com>
2024-10-15 11:44:04 +0700

    Delete frontend directory

--- frontend/src/utils/url-utils.js
+++
@@ -1,7 +0,0 @@
-export class UrlUtils {
-    static getUrlParam(param) {
-        //Достаем id пользователя из url адреса
-        const urlParams = new URLSearchParams(window.location.search)
-        return urlParams.get(param);
-    }
-}
